e194bc2ecb842775479ed83c060b15f18489d4ef
Luigi <l.avila.uvg@gmail.com>
2023-10-25 17:43:23 -0400

    feat: migration to version catalogs completed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath(libs.google.services)
     }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("com.android.library") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
 }
+true
